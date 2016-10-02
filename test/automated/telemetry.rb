082ef33f2557b2be7deee95926838790cd881fab
Scott Bellware <sbellware@gmail.com>
2016-10-02 01:06:12 -0500

    Telemetry tests

--- test/automated/telemetry.rb
+++ test/automated/telemetry.rb
@@ -3,8 +3,8 @@ require_relative 'automated_init'
 context "Cycle" do
   context "Telemetry" do
     context "Got Result" do
-      cycle = Iterator::Cycle.build
-      sink = Iterator::Cycle.register_telemetry_sink(cycle)
+      cycle = Cycle.build(delay_milliseconds: 1, timeout_milliseconds: 2)
+      sink = Cycle.register_telemetry_sink(cycle)
 
       cycle.() do
         'some result'
@@ -12,7 +12,7 @@ context "Cycle" do
 
       test "Recorded cycle" do
         iteration = 0
-        assert(sink.recorded_cycle? { |r| r.data == iteration })
+        assert(sink.recorded_cycle? { |record| record.data == iteration })
       end
 
       test "Recorded invoked action" do
@@ -33,15 +33,16 @@ context "Cycle" do
     end
 
     context "Got No Result" do
-      cycle = Iterator::Cycle.build(delay_milliseconds: 50, timeout_milliseconds: 100)
-      sink = Iterator::Cycle.register_telemetry_sink(cycle)
+      cycle = Cycle.build(delay_milliseconds: 1, timeout_milliseconds: 2)
+      sink = Cycle.register_telemetry_sink(cycle)
 
       cycle.() do
         nil
       end
 
       test "Recorded cycle" do
-        assert(sink.recorded_cycle?)
+        iteration = 0
+        assert(sink.recorded_cycle? { |record| record.data == iteration })
       end
 
       test "Recorded invoked action" do
@@ -49,11 +50,11 @@ context "Cycle" do
       end
 
       test "Recorded delayed" do
-        assert(sink.recorded_delayed?)
+        assert(sink.recorded_delayed? { |record| record.data == 1 })
       end
 
       test "Recorded timed out" do
-        assert(sink.recorded_timed_out?)
+        assert(sink.recorded_timed_out? { |record| record.data < Clock::UTC.now })
       end
 
       test "Didn't record got result" do
