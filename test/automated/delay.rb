670c490bedc9c790bfe6377aa4d496f99ac4d81a
Nathan Ladd <nathan@obsidianexchange.com>
2017-01-19 13:15:11 -0600

    Delay test is clarified

--- test/automated/delay.rb
+++ test/automated/delay.rb
@@ -17,6 +17,8 @@ context "Cycle" do
 
     context "Elapsed time of action is within maximum milliseconds" do
       cycle = Cycle.build(maximum_milliseconds: 11, timeout_milliseconds: 2)
+      elapsed_milliseconds = 10
+
       sink = Cycle.register_telemetry_sink(cycle)
 
       clock = SubstAttr::Substitute.(:clock, cycle)
@@ -24,19 +26,23 @@ context "Cycle" do
       clock.now = Controls::Time::Raw.example
 
       action = proc {
-        clock.now = Controls::Time::Offset::Raw.example 10
+        clock.now = Controls::Time::Offset::Raw.example elapsed_milliseconds
         nil
       }
 
       cycle.(&action)
 
       test "Cycle is delayed by maximum milliseconds less elapsed time" do
-        assert(sink.recorded_delayed? { |record| record.data == 1 })
+        delay_milliseconds = cycle.maximum_milliseconds - elapsed_milliseconds
+
+        assert(sink.recorded_delayed? { |record| record.data == delay_milliseconds })
       end
     end
 
     context "Elapsed time of action exceeds maximum milliseconds" do
       cycle = Cycle.build(maximum_milliseconds: 11, timeout_milliseconds: 2)
+      elapsed_milliseconds = 11
+
       sink = Cycle.register_telemetry_sink(cycle)
 
       clock = SubstAttr::Substitute.(:clock, cycle)
@@ -44,7 +50,7 @@ context "Cycle" do
       clock.now = Controls::Time::Raw.example
 
       action = proc {
-        clock.now = Controls::Time::Offset::Raw.example 11
+        clock.now = Controls::Time::Offset::Raw.example elapsed_milliseconds
         nil
       }
 
