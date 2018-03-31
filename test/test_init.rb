a7f45ddd8083eb3e4041acabe0ab62c71fc44694
achernik <achernik@gmail.com>
2018-03-31 19:04:14 +0300

    Removed configuration of unused Log ENV variables in tests

--- test/test_init.rb
+++ test/test_init.rb
@@ -1,14 +1,5 @@
 ENV['CONSOLE_DEVICE'] ||= 'stdout'
-ENV['LOG_COLOR'] ||= 'on'
-
-if ENV['LOG_LEVEL']
-  ENV['LOGGER'] ||= 'on'
-else
-  ENV['LOG_LEVEL'] ||= 'trace'
-end
-
-ENV['LOGGER'] ||= 'off'
-ENV['LOG_OPTIONAL'] ||= 'on'
+ENV['LOG_LEVEL'] ||= 'trace'
 
 puts RUBY_DESCRIPTION
 
