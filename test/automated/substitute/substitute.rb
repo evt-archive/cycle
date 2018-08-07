60cbde208bd9bd77393020882cb797c65211b627
Scott Bellware <sbellware@gmail.com>
2018-08-07 16:10:29 -0500

    Substitute tests

--- test/automated/substitute/substitute.rb
+++ test/automated/substitute/substitute.rb
@@ -1,3 +1,13 @@
 require_relative '../automated_init'
 
+context "Substitute" do
+  cycle = Cycle::Substitute.build
 
+  test "Timeout time is 0" do
+    assert(cycle.timeout_milliseconds == 0)
+  end
+
+  test "Telemetry sink is activated" do
+    refute(cycle.telemetry_sink.nil?)
+  end
+end
